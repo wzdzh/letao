4aae448caba3ba7e5b3fe1e8fb66b4f83b0054cb
wzd <1243312440@qq.com>
2017-10-30 20:28:52 +0800

    提交后台主页

--- letao/public/back/js/common.js
+++ letao/public/back/js/common.js
@@ -2,10 +2,45 @@
  * Created by Administrator on 2017/10/29.
  */
 $(function(){
+  if(location.href.indexOf("login.html") === -1){
+    $.ajax({
+      type:"get",
+      url:" /employee/checkRootLogin",
+      success:function(data){
+        if(data.error === 400){
+          location.href = "login.html";
+        }
+      }
+    })
+  }
   $(document).ajaxStart(function(){
     NProgress.start();
   });
   $(document).ajaxSuccess(function(){
     NProgress.done();
+  },500);
+
+
+  $(".icon_loginout").on("click",function(){
+    $(".modal").modal("show");
+  });
+
+  $(".father").on("click",function(){
+    $(".child").slideToggle();
+  });
+
+  $(".icon_menu").on("click",function(){
+    $(".lt_aside,.lt_main,.lt_header").toggleClass("active");
+  });
+  $(".login_out").on("click",function(){
+    $.ajax({
+      type:"get",
+      url: "/employee/employeeLogout",
+      success:function(data){
+      if(data.success){
+        location.href = "login.html";
+      }
+    }
+    })
   })
 });
---
+++ letao/public/back/js/index.js
@@ -0,0 +1,74 @@
+/**
+ * Created by Administrator on 2017/10/29.
+ */
+$(function(){
+  var myChart = echarts.init(document.querySelector(".chart_left"));
+
+  // 指定图表的配置项和数据
+  var option = {
+    title: {
+      text: '2017注册人数'
+    },
+    tooltip: {},
+    legend: {
+      data:['销量']
+    },
+    xAxis: {
+      data: ["1月","2月","3月","4月","5月","6月"]
+    },
+    yAxis: {},
+    series: [{
+      name: '销量',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }]
+  };
+  myChart.setOption(option);
+
+  var myChart1 = echarts.init(document.querySelector(".chart_right"));
+  option1 = {
+    title : {
+      text: '热门品牌销售',
+      subtext: '2017年6月',
+      x:'center'
+    },
+    tooltip : {
+      trigger: 'item',
+      formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left',
+      data: ['阿迪','耐克','李宁','安踏','匹克']
+    },
+    series : [
+      {
+        name: '访问来源',
+        type: 'pie',
+        radius : '55%',
+        center: ['50%', '60%'],
+        data:[
+          {value:335, name:'阿迪'},
+          {value:310, name:'耐克'},
+          {value:234, name:'李宁'},
+          {value:135, name:'安踏'},
+          {value:1548, name:'匹克'}
+        ],
+        itemStyle: {
+          emphasis: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
+  myChart1.setOption(option1);
+
+  $(window).on("resize",function(){
+    var width = $(window).width();
+    var headerWidth = width - 180;
+    $(".lt_header"). css({width:headerWidth});
+  }).trigger("resize");
+});
