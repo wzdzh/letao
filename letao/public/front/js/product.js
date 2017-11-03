a47091933516d471d21be3cf4aebbfe609e67171
wzd <1243312440@qq.com>
2017-11-03 23:14:30 +0800

    提交商品信息功能

--- letao/public/front/js/product.js
+++ letao/public/front/js/product.js
@@ -6,5 +6,27 @@ $(function(){
     deceleration: 0.0005,
     indicators:false
   });
+ var id = tools.getParam("productId");
 
+  $.ajax({
+    type:"get",
+    url:"/product/queryProductDetail",
+    data:{id:id},
+    success:function(data){
+      console.log(data);
+      var arr = data.size.split("-");
+      var sizeArr = [];
+      console.log(arr);
+      for(var i = +arr[0];i <= +arr[1];i++){
+           sizeArr.push(i);
+      }
+      data.size = sizeArr;
+      console.log(data);
+      $(".mui-scroll").html(template("tpl",data));
+      mui('.mui-slider').slider({
+        interval:5000
+      });
+      mui(".mui-numbox").numbox();
+    }
+  })
 });
